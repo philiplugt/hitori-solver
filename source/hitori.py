4589404ae6e0aab54a4b3c77e0f7f451bff92a20
pxv8780 <philiplugt@netscape.net>
2024-01-01 22:06:21 +0100

    Fixed DFS traversal mistake, and made display compatible with larger digit numbers

--- source/hitori.py
+++ source/hitori.py
@@ -115,7 +115,13 @@ def solve_smart(puzzle):
     def check_solution(state, domain):
         return (domain_complete(domain, True) and test_duplicate_number(state))
 
+    matrix = []
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            matrix.append([i, j])
+
     idx = 1
+    iterr = [0]
     initial_node = [0, 0]
     nodes = [initial_node]
     intial_state = [row[:] for row in puzzle]
@@ -126,6 +132,7 @@ def solve_smart(puzzle):
     visited_list = [initial_visited]
 
     while nodes:
+        index = iterr.pop()
         i, j = nodes.pop()
         state = stack.pop()
         domain = domains.pop()
@@ -155,13 +162,21 @@ def solve_smart(puzzle):
             # Label cell as B (black) or W (white) if not yet labeled
             for d in domain[i][j]:
 
+
                 # Unique white (V) cells are final, thus proceed to the next node
                 if d == 'V' :
                     new_visited_unique = [row[:] for row in visited]
                     new_visited_unique[i][j] = True
                     
-                    for xy in adjacent_cells:                
-                        nodes.append(xy)
+                    if index+1 < len(matrix):
+                        iterr.append(index+1)
+                        nodes.append(matrix[index+1])
+                        stack.append(state)
+                        domains.append(domain)
+                        visited_list.append(new_visited_unique)
+                    else:
+                        iterr.append(index)
+                        nodes.append(matrix[index])
                         stack.append(state)
                         domains.append(domain)
                         visited_list.append(new_visited_unique)
@@ -180,19 +195,18 @@ def solve_smart(puzzle):
                         continue
 
                     # MRV, add the smallest domains to the stack last so they get handled first
-                    for x, y in adjacent_cells:
-                        if len(new_domain_white[x][y]) > 1:
-                            nodes.append([x, y])
-                            stack.append(new_state_white)
-                            domains.append(new_domain_white)
-                            visited_list.append(new_visited_white)
-
-                    for x, y in adjacent_cells:
-                        if len(new_domain_white[x][y]) == 1:
-                            nodes.append([x, y])
-                            stack.append(new_state_white)
-                            domains.append(new_domain_white)
-                            visited_list.append(new_visited_white)
+                    if index+1 < len(matrix):
+                        iterr.append(index+1)
+                        nodes.append(matrix[index+1])
+                        stack.append(new_state_white)
+                        domains.append(new_domain_white)
+                        visited_list.append(new_visited_white)
+                    else:
+                        iterr.append(index)
+                        nodes.append(matrix[index])
+                        stack.append(new_state_white)
+                        domains.append(new_domain_white)
+                        visited_list.append(new_visited_white)
 
                 # Black label is only given if conditions are met
                 if d == 'B' and black_allowed(state, i, j):
@@ -210,8 +224,15 @@ def solve_smart(puzzle):
                         if not new_domain_black:
                             continue
 
-                        for x, y in adjacent_cells:            
-                            nodes.append([x, y])
+                        if index+1 < len(matrix):
+                            iterr.append(index+1)
+                            nodes.append(matrix[index+1])
+                            stack.append(new_state_black)
+                            domains.append(new_domain_black)
+                            visited_list.append(new_visited_black)
+                        else:
+                            iterr.append(index)
+                            nodes.append(matrix[index])
                             stack.append(new_state_black)
                             domains.append(new_domain_black)
                             visited_list.append(new_visited_black)
@@ -220,18 +241,25 @@ def solve_smart(puzzle):
 
 # Prints a 2d array for display
 def display(x):
-    start_bar = "─" * (len(x[0]) * 2 + 1)
-    end_bar = "─" * (len(x[len(x)-1]) * 2 + 1)
+    # Get the size of the largest digits, to determine width for printing grid
+    big = 0
+    for i in x:
+        for j in i:
+            big = max(big, len(str(j)))
+    digits = big
+
+    start_bar = "─" * (len(x[0]) * 2 + 1) + "─" * (len(x[0]) * (digits-1))
+    end_bar = "─" * (len(x[len(x)-1]) * 2 + 1) + "─" * (len(x[len(x)-1]) * (digits-1))
     print(f"  ┌{start_bar}┐")
     for i in x:
         print("  │ ", end="")
         for j in i: 
             if isinstance(j, int):
-                char = f"{str(j)} "
+                char = f"{str(j).rjust(digits)} "
             elif j == "B":
-                char = "█ "
+                char = "█ ".rjust(digits+1)
             else:
-                char = f"{j} "
+                char = f"{j} ".rjust(digits+1)
             print(char, end="")
         print("│")
     print(f"  └{end_bar}┘")
@@ -316,10 +344,49 @@ if __name__ == "__main__":
         [2, 3, 2, 4, 5],
     ]
 
+    puzzle11 = [
+        [2, 9, 3, 2, 8, 5, 4, 7, 3, 10, 8, 11],
+        [2, 7, 10, 6, 7, 4, 1, 10, 1, 9, 8, 12],
+        [11, 6, 12, 11, 5, 4, 2, 12, 4, 6, 1, 1],
+        [4, 10, 2, 11, 9, 6, 12, 4, 7, 5, 5, 1],
+        [6, 12, 4, 12, 10, 1, 2, 5, 3, 11, 2, 3],
+        [9, 4, 4, 5, 7, 6, 6, 12, 7, 2, 12, 9],
+        [4, 2, 8, 4, 3, 10, 7, 2, 11, 12, 9, 5],
+        [10, 10, 7, 9, 4, 1, 11, 2, 9, 1, 12, 7],
+        [5, 1, 8, 4, 4, 12, 8, 10, 2, 5, 10, 3],
+        [5, 3, 11, 9, 12, 9, 10, 1, 2, 4, 5, 2],
+        [9, 6, 10, 12, 2, 9, 1, 1, 5, 12, 11, 4],
+        [10, 11, 11, 10, 3, 2, 5, 8, 9, 3, 2, 7],
+    ]
+
+    puzzle12 = [
+        [7, 15, 11, 11, 5, 3, 13, 1, 6, 13, 17, 5, 18, 12, 7, 14, 16, 8, 4, 6], 
+        [1, 16, 9, 7, 8, 19, 2, 14, 13, 2, 20, 4, 12, 10, 5, 8, 15, 16, 6, 5], 
+        [20, 1, 1, 11, 7, 13, 3, 5, 8, 9, 15, 10, 13, 16, 11, 3, 6, 4, 15, 6], 
+        [8, 6, 13, 3, 15, 4, 17, 19, 10, 4, 18, 8, 2, 8, 7, 5, 20, 10, 10, 7], 
+        [4, 11, 17, 15, 1, 12, 19, 13, 10, 16, 6, 5, 16, 19, 2, 3, 7, 15, 14, 1], 
+        [18, 8, 2, 1, 19, 1, 16, 20, 4, 6, 11, 17, 10, 13, 17, 7, 12, 12, 1, 14], 
+        [17, 8, 6, 10, 18, 5, 19, 17, 3, 17, 1, 5, 7, 2, 13, 4, 8, 14, 16, 5], 
+        [5, 13, 18, 11, 9, 15, 7, 11, 19, 1, 4, 14, 7, 6, 9, 2, 6, 16, 4, 3], 
+        [8, 18, 3, 1, 1, 14, 7, 2, 13, 12, 2, 20, 10, 10, 6, 13, 4, 15, 19, 15], 
+        [15, 9, 14, 19, 2, 10, 9, 16, 6, 14, 7, 1, 3, 14, 17, 2, 13, 5, 15, 12], 
+        [19, 2, 12, 20, 12, 6, 10, 7, 18, 16, 5, 11, 8, 14, 19, 14, 1, 13, 17, 4], 
+        [2, 16, 4, 10, 17, 1, 1, 8, 3, 15, 14, 16, 10, 5, 10, 9, 9, 20, 17, 19], 
+        [13, 18, 1, 4, 16, 8, 18, 12, 15, 7, 11, 3, 9, 3, 20, 19, 6, 17, 5, 15], 
+        [11, 5, 7, 16, 4, 6, 11, 6, 10, 18, 14, 3, 13, 15, 1, 9, 19, 10, 20, 8], 
+        [11, 19, 20, 15, 3, 16, 9, 17, 2, 11, 10, 12, 4, 16, 18, 15, 2, 6, 9, 1], 
+        [10, 4, 15, 6, 7, 5, 13, 7, 18, 17, 2, 14, 14, 9, 18, 10, 3, 2, 1, 20], 
+        [7, 17, 11, 12, 20, 4, 14, 13, 16, 5, 5, 13, 11, 1, 10, 5, 12, 2, 3, 17], 
+        [9, 9, 13, 14, 13, 7, 17, 4, 1, 10, 19, 11, 15, 8, 12, 17, 18, 3, 6, 16], 
+        [6, 20, 11, 17, 8, 7, 15, 9, 12, 10, 3, 19, 15, 4, 14, 1, 5, 4, 2, 18], 
+        [5, 18, 16, 12, 14, 17, 18, 18, 18, 19, 13, 9, 13, 20, 12, 1, 2, 15, 8, 13], 
+    ]
+
+
     # Method to use: "True" for brute force; "False" for smart (CSP with Forward checking/MRV)
     method = False
 
     # Set a puzzle
-    puzzle = puzzle9
+    puzzle = puzzle8
 
     solve_hitori(puzzle, method)
